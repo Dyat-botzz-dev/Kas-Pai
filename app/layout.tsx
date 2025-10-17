f1ebfa023d2b05891bc5b46ae92d3aceb642dfd5
Xyz-King's <xyzkings22@gmail.com>
2025-10-17 15:13:37 +0800

    update app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Kas PAI',
+  description: 'Created XyzKings',
+  generator: 'XyzKings',
   icons: {
     icon: 'https://files.catbox.moe/wozyle.jpg',
   },
